28c619e275e6766c5571ca914049329ce641c148
Nathan Bourgeois <NT-Bourgeois-Iridescence-Technologies@users.noreply.github.com>
2019-06-25 21:02:15 +0200

    Revert "Revert reversion of sun"
    This reverts commit 2d147a181610d93b561d9cb89b0397992e91f3ea.

--- Minecraft/Engine/Aurealis/Graphics/Effects/SkyLight.cpp
+++ Minecraft/Engine/Aurealis/Graphics/Effects/SkyLight.cpp
@@ -11,7 +11,6 @@ namespace Aurealis
 		SkyLight::SkyLight()
 		{
 			skyVertices = (TexturedVertex*)memalign(16,4 * sizeof(TexturedVertex));
-			isMoon = false;
 		}
 
 		SkyLight::~SkyLight()
@@ -19,87 +18,49 @@ namespace Aurealis
 			free(skyVertices);
 		}
 
-		void  SkyLight::UpdateLightSource(float sun_angle, int day, bool moon)
+		void  SkyLight::UpdateLightSource(float sun_angle)
 		{
-			isMoon = moon;
-			float xMin, yMin, xMax, yMax;
-			xMin = yMin = xMax = yMax = 0;
-			if (moon) {
-				int phase = day % 8;
-
-				int xpos = day % 4;
-				int ypos = 0;
-				if (day < 4) {
-					ypos = 0;
-				}
-				else {
-					ypos = 1;
-				}
-
-				xMin = 0.25f * xpos;
-				yMin = 0.5f * ypos;
-
-				xMax = 0.25f * xpos + 0.25f;
-				yMax = 0.5f * ypos + 0.5f;
-			}
-			else {
-				xMin = 0.f;
-				yMin = 0.f;
-				xMax = 1.f;
-				yMax = 1.f;
-			}
-
-			float r = 0.0f;
-			
-			if (moon) {
-				r = 670.0f*2;
-			}
-			else {
-				r = 670.0f*2;
-			}
+			float r = 670.0f;
 			float shift = 325;
 			//float textureScale = 1.0f / stepScale;
 
 			int i = 0;
 
-			skyVertices[i].x = vfpu_sinf((sun_angle / 180) * PI) * r;// * stepScale;
-			skyVertices[i].y = vfpu_cosf(((sun_angle) / 180) * PI) * -r;// * stepScale;
-			skyVertices[i].z = -500 * 0.9f;// * stepScale;
-			skyVertices[i].u = xMin;// * textureScale;
-			skyVertices[i].v = yMin;// * textureScale;
+			skyVertices[i].x = vfpu_sinf((sun_angle/180)*PI)*r;// * stepScale;
+			skyVertices[i].y = vfpu_cosf(((sun_angle)/180)*PI)*-r;// * stepScale;
+			skyVertices[i].z = -250*0.9f;// * stepScale;
+			skyVertices[i].u = 0.f;// * textureScale;
+			skyVertices[i].v = 0.f;// * textureScale;
 			i++;
 
 			// (x, y - 1, z)
-			skyVertices[i].x = vfpu_sinf(((sun_angle - 45 * 0.9f) / 180) * PI) * r;// * stepScale;
-			skyVertices[i].y = vfpu_cosf(((sun_angle - 45 * 0.9f) / 180) * PI) * -r;// * stepScale;
-			skyVertices[i].z = -500 * 0.9f;// * stepScale;
-			skyVertices[i].u = xMin;// * textureScale;
-			skyVertices[i].v = yMax;// * textureScale;
+			skyVertices[i].x = vfpu_sinf(((sun_angle-45*0.9f)/180)*PI)*r;// * stepScale;
+			skyVertices[i].y = vfpu_cosf(((sun_angle-45*0.9f)/180)*PI)*-r;// * stepScale;
+			skyVertices[i].z = -250*0.9f;// * stepScale;
+			skyVertices[i].u = 0.f;// * textureScale;
+			skyVertices[i].v = 1.f;// * textureScale;
 			i++;
 
 			// (x + 1, y, z)
-			skyVertices[i].x = vfpu_sinf((sun_angle / 180) * PI) * r;// * stepScale;
-			skyVertices[i].y = vfpu_cosf((sun_angle / 180) * PI) * -r;// * stepScale;
-			skyVertices[i].z = 500 * 0.9f;// * stepScale;
-			skyVertices[i].u = xMax;// * textureScale;
-			skyVertices[i].v = yMin;// * textureScale;
+			skyVertices[i].x = vfpu_sinf((sun_angle/180)*PI)*r;// * stepScale;
+			skyVertices[i].y = vfpu_cosf((sun_angle/180)*PI)*-r;// * stepScale;
+			skyVertices[i].z = 250*0.9f;// * stepScale;
+			skyVertices[i].u = 1.f;// * textureScale;
+			skyVertices[i].v = 0.f;// * textureScale;
 			i++;
 
 			// (x + 1, y - 1, z)
-			skyVertices[i].x = vfpu_sinf(((sun_angle - 45 * 0.9f) / 180) * PI) * r;// * stepScale;
-			skyVertices[i].y = vfpu_cosf(((sun_angle - 45 * 0.9f) / 180) * PI) * -r;// * stepScale;
-			skyVertices[i].z = 500 * 0.9f;// * stepScale;
-			skyVertices[i].u = xMax;// * textureScale;
-			skyVertices[i].v = yMax;// * textureScale;
+			skyVertices[i].x = vfpu_sinf(((sun_angle-45*0.9f)/180)*PI)*r;// * stepScale;
+			skyVertices[i].y = vfpu_cosf(((sun_angle-45*0.9f)/180)*PI)*-r;// * stepScale;
+			skyVertices[i].z = 250*0.9f;// * stepScale;
+			skyVertices[i].u = 1.f;// * textureScale;
+			skyVertices[i].v = 1.f;// * textureScale;
 
-			sceKernelDcacheWritebackInvalidateRange(skyVertices, 4 * sizeof(TexturedVertex));
+			sceKernelDcacheWritebackInvalidateRange(skyVertices,4 * sizeof(TexturedVertex));
 		}
 
 		void SkyLight::Render()
 		{
-			sceGumPushMatrix();
-			sceGumRotateX(40.0f / 180.0f * 3.14159f);
-			
 			sceGuEnable(GU_TEXTURE_2D);
 			sceGuEnable(GU_BLEND);
 
@@ -111,7 +72,6 @@ namespace Aurealis
 
 			//make default blend function
 			sceGuBlendFunc(GU_ADD, GU_SRC_ALPHA, GU_ONE_MINUS_SRC_ALPHA, 0, 0);
-			sceGumPopMatrix();
 		}
 	}
 }
