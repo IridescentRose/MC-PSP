0d7f76978118dab63c62b5265598f5f1a36e5e20
Nathan Bourgeois <NT-Bourgeois-Iridescence-Technologies@users.noreply.github.com>
2019-08-20 08:09:28 -0400

    Default options

--- Minecraft/Minecraft/Common/Options.cpp
+++ Minecraft/Minecraft/Common/Options.cpp
@@ -26,7 +26,7 @@ namespace Minecraft::Common{
 
     }
 
-    Options g_DefaultOptions = {};
+    Options g_DefaultOptions = {1343, false, 0.5f, 0.0f, 0.0f, 0.0f, 4, 0, 0, true, 60, false, 2, true, 2, 2, true, "", "", "", "en_us", 0, true, true, true, 1.0f, true, false, false, false, false, false, true, false, 0, 0, true, 1.0f, 0.44366196f, 1.0f, 1.0f, 4, false, false, true, true, true, 1, false, true, true, 0, "movement", true, 1.0f, 1.0f, 1.0f, 1.0f, 1.0f, 1.0f, 1.0f, 1.0f, 1.0f, 1.0f, 1.0f, true, true, true, true, true, true, true};
 
     OptionsManager g_OptionsManager = OptionsManager();
 }
