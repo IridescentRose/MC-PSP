1b3a5b0a26e7bf0039328837c311eb64e3ae9608
Nathan Bourgeois <iridescentrosesfall@gmail.com>
2019-09-21 17:24:54 +0000

    Added MCLIB Core

---
+++ Minecraft/libs/include/common/Common.h
@@ -0,0 +1,14 @@
+#ifndef MCLIB_COMMON_COMMON_H_
+#define MCLIB_COMMON_COMMON_H_
+
+#include <memory>
+
+#include "../mclib.h"
+#include "Types.h"
+#include "Vector.h"
+#include "DataBuffer.h"
+#include "MCString.h"
+#include "VarInt.h"
+#include "UUID.h"
+
+#endif
