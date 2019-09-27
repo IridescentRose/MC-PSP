bcc60381d46665b4d1db503cb034b1da9123a129
Nathan Bourgeois <IridescentRosesFall@gmail.com>
2019-09-27 00:48:25 -0400

    MCLib Client.

--- Minecraft/Minecraft/Client/GameClient.cpp
+++ Minecraft/Minecraft/Client/GameClient.cpp
@@ -3,6 +3,16 @@
 #include <Shadow/System/Ram.h>
 #include <sstream>
 
+#include "../Common/Options.hpp"
+
+#include <mclib/mclib.h>
+#include <mclib/common/Common.h>
+#include <mclib/core/Client.h>
+#include <mclib/util/Forge.h>
+#include <mclib/util/Hash.h>
+#include <mclib/util/Utility.h>
+#include <mclib/util/VersionFetcher.h>
+
 namespace Minecraft::Client {
 
 	ClientState::ClientState() {
@@ -13,8 +23,42 @@ namespace Minecraft::Client {
 	}
 
 	void ClientState::Init() {
+		
+		Logging::warn("Connecting!");
 		//CONNECT TEST, DON'T START ALLOCATING RAM UNNECESSARILY!
+		mc::util::VersionFetcher versionFetcher(connect_info.ip_address, 25565);
 
+		Logging::warn("Fetched version!");
+		mc::protocol::Version version = versionFetcher.GetVersion();
+
+		mc::block::BlockRegistry::GetInstance()->RegisterVanillaBlocks();
+
+		Logging::log("Connecting with version " + mc::protocol::to_string(version), Logging::LOGGER_LEVEL_INFO);
+		
+		mc::protocol::packets::PacketDispatcher dispatcher;
+		mc::core::g_Client = new mc::core::Client(&dispatcher, version);
+
+		mc::util::ForgeHandler& forge = versionFetcher.GetForge();
+
+		forge.SetConnection(mc::core::g_Client->GetConnection());
+
+		mc::core::g_Client->GetPlayerController()->SetHandleFall(true);
+
+		//Set more as needed
+		mc::core::g_Client->GetConnection()->GetSettings()
+			.SetMainHand(Common::g_OptionsManager.options.mainHand ? mc::MainHand::Right : mc::MainHand::Left)
+			.SetViewDistance(Common::g_OptionsManager.options.renderDistance)
+			.SetChatColors(Common::g_OptionsManager.options.chatColors);
+
+		
+		try {
+			Logging::log("Logging in.", Logging::LOGGER_LEVEL_INFO);
+			mc::core::g_Client->Login(connect_info.ip_address, 25565, connect_info.username, "", mc::core::UpdateMethod::Threaded);
+		}
+		catch (std::exception& e) {
+			Logging::log(e.what(), Logging::LOGGER_LEVEL_FATAL);
+			sceKernelExitGame();
+		}
 	}
 	void ClientState::Enter() {
 		u32 ramFree = System::freeMemory();
@@ -40,7 +84,14 @@ namespace Minecraft::Client {
 	}
 
 	void ClientState::Update(StateManager* sManager) {
+		u32 ramFree = System::freeMemory();
+		float ram = ((float)ramFree) / 1024.0f / 1024.0f;
+		std::ostringstream os;
+		os << ram;
+		std::string s(os.str());
 
+
+		Logging::log("RAM: " + s, Logging::LOGGER_LEVEL_TRACE);
 	}
 	void ClientState::Draw(StateManager* sManager) {
 
