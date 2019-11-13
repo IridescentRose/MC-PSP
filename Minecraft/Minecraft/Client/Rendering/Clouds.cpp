a33b9d388345d1e53467b558e7de5e22f628b6ce
Nathan Bourgeois <IridescentRosesFall@gmail.com>
2019-11-12 23:29:25 -0500

    Fixed some cloud issues - still renders weirdly.

--- Minecraft/Minecraft/Client/Rendering/Clouds.cpp
+++ Minecraft/Minecraft/Client/Rendering/Clouds.cpp
@@ -63,13 +63,35 @@ namespace Minecraft::Rendering {
 		sceGuDisable(GU_CLIP_PLANES);
 		
 		sceGumPushMatrix();
-		ScePspFVector3 v = { pos.x + 0.0f, 0.0f, pos.z + (dt % 25600 + 1) * 2400.0f / 25600.0f };
+		ScePspFVector3 v = { pos.x + 0.0f, 0.0f, pos.z + (dt % 5460) * 2400.0f / 25600.0f };
 		sceGumTranslate(&v);
 
 		
 		sceGuTexScale(0.25f, 0.25f);
 		tex->bindTextureRepeat(0, 0, true);
-		sceGuColor(GU_COLOR(1.0f, 1.0f, 1.0f, 0.6f));
+
+		float alpha = 0.7f;
+
+		if (dt < 2000) { //6am - 9am sun gets brighter
+			alpha = 0.5f + 0.2f * ((float)dt / 2000.f);
+		}
+		if (dt >= 2000 && dt < 10000) { //9am - 3pm sun is brightest
+			alpha = 0.7f;
+		}
+		if (dt >= 10000 && dt < 12000) { //3pm - 6pm sun gets dimmer
+			alpha = 0.7f - 0.2f * (((float)dt - 10000.f) / 2000.f);
+		}
+		if (dt >= 12000 && dt < 14000) { //6pm - 9pm sun light fades
+			alpha = 0.5f - 0.2f * (((float)dt - 12000.f) / 2000.f);
+		}
+		if (dt >= 14000 && dt < 22000) {//9pm - 3am is night
+			alpha = 0.3f;
+		}
+		if (dt >= 22000 && dt < 24000) {
+			alpha = 0.3f + 0.2f * (((float)dt - 22000) / 2000);
+		}
+
+		sceGuColor(GU_COLOR(1.0f, 1.0f, 1.0f, alpha));
 
 		sceGumDrawArray(GU_TRIANGLE_STRIP, GU_TEXTURE_32BITF | GU_VERTEX_32BITF | GU_TRANSFORM_3D, 4, 0, cloudVerts);
 
