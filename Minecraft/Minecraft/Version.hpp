595140451056e31aeb5e1473c98eb3fa379e04c6
Nathan Bourgeois <IridescentRosesFall@gmail.com>
2020-01-14 14:18:41 -0500

    Update Version.hpp

--- Minecraft/Minecraft/Version.hpp
+++ Minecraft/Minecraft/Version.hpp
@@ -4,7 +4,7 @@
 #define EMULATED_VERSION "1.12.2"
 
 #define IS_SNAPSHOT false
-#define INTERNAL_VERSION "0.3-pre4"
+#define INTERNAL_VERSION "0.3"
 #define INTERNAL_SNAPSHOT "19w34a"
 
 #define VERSION_MAJOR 0
