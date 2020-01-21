9654e6120fd909c8c2fb5fe4fe20524d83bf5c3f
Nathan Bourgeois <iridescentrosesfall@gmail.com>
2020-01-21 03:36:17 -0500

    Forced EBOOT tracking.

--- build.sh
+++ build.sh
@@ -1,5 +1,8 @@
 cd Minecraft
 make -j16 -f MakefileGame
+cp EBOOT.PBP ../Game/
+cp Minecraft.prx ../Game/
+cp Minecraft.elf ../Game/
 mv EBOOT.PBP ../../Game/
 mv Minecraft.prx ../../Game/
 mv Minecraft.elf ../../Game/
