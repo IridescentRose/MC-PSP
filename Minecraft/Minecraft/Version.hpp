1415930c81a1e796fd9bd653bd93ae8f59005c48
Nathan Bourgeois <iridescentrosesfall@gmail.com>
2020-01-24 04:23:06 -0500

    Versioning fix

--- Minecraft/Minecraft/Version.hpp
+++ Minecraft/Minecraft/Version.hpp
@@ -3,8 +3,8 @@
 #define GAME_NAME "Minecraft"
 #define EMULATED_VERSION "1.12.2"
 
-#define IS_SNAPSHOT true
-#define INTERNAL_VERSION "0.3"
+#define IS_SNAPSHOT false
+#define INTERNAL_VERSION "0.4-pre1"
 #define INTERNAL_SNAPSHOT "20w4c"
 
 #define VERSION_MAJOR 0
