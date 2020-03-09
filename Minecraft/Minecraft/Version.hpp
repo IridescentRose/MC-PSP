80f688104bbb12e728e25224e2959ed93da050a8
Nathan Bourgeois <iridescentrosesfall@gmail.com>
2020-03-09 19:52:00 -0400

    Versioning

--- Minecraft/Minecraft/Version.hpp
+++ Minecraft/Minecraft/Version.hpp
@@ -3,9 +3,9 @@
 #define GAME_NAME "Minecraft"
 #define EMULATED_VERSION "1.12.2"
 
-#define IS_SNAPSHOT false
+#define IS_SNAPSHOT true
 #define INTERNAL_VERSION "0.4"
-#define INTERNAL_SNAPSHOT "20w9b"
+#define INTERNAL_SNAPSHOT "20w11a"
 
 #define VERSION_MAJOR 0
 #define VERSION_MINOR 3
