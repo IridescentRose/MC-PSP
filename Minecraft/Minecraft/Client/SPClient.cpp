632ca5e725e841e5e18cda9ba27764c8ae3e5f19
Nathan Bourgeois <iridescentrosesfall@gmail.com>
2020-03-09 17:38:19 -0400

    Revert to fix problems

--- Minecraft/Minecraft/Client/SPClient.cpp
+++ Minecraft/Minecraft/Client/SPClient.cpp
@@ -23,7 +23,7 @@ namespace Minecraft::Client {
 		
 		updateTimer.reset();
 		player = new Player();
-		player->Init({ -128000, 69.0f, -128000}, 0, 0);
+		player->Init({ -32000, 69.0f, -32000}, 0, 0);
 		
 		//CREATIVE
 		player->toggleFly();
