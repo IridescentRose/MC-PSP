dc11076a45574daf3cbb5267b4a9ab78598d043d
Nathan Bourgeois <iridescentrosesfall@gmail.com>
2020-03-13 21:45:48 -0400

    Versioning

--- Minecraft/Minecraft/Version.hpp
+++ Minecraft/Minecraft/Version.hpp
@@ -5,7 +5,7 @@
 
 #define IS_SNAPSHOT true
 #define INTERNAL_VERSION "0.4"
-#define INTERNAL_SNAPSHOT "20w11b"
+#define INTERNAL_SNAPSHOT "20w11c"
 
 #define VERSION_MAJOR 0
-#define VERSION_MINOR 3
+#define VERSION_MINOR 4
