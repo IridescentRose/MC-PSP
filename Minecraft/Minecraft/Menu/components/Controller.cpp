744e45250e0fa2e60b0dafe0724435b392bb3c94
Nathan Bourgeois <iridescentrosesfall@gmail.com>
2020-03-16 21:04:26 -0400

    Selections

--- Minecraft/Minecraft/Menu/components/Controller.cpp
+++ Minecraft/Minecraft/Menu/components/Controller.cpp
@@ -24,11 +24,53 @@ namespace Minecraft::Menus{
         unselected->SetPosition(240, 256);
         unselected->Draw();
 
+        if(selectPosY == 16){
+            selected->SetPosition(240, 256);
+            selected->Draw();
+        }else{
+            opt_sel->SetPosition( 120 + 240 * (selectPosY / 8), 48 + 24*(selectPosY % 8) );
+            opt_sel->Draw();
+        }
 
+
+        g_RenderManager.DebugPrint(240, 24, Common::g_TranslationOBJ.getText("controls.title").c_str());
+
+        if(selectPosY == 16){
+            g_RenderManager.SetFontStyle(PSP_MENU_SIZE, 0xFF77FFFF, 0, INTRAFONT_ALIGN_CENTER, 0.0f);
+        }else{
+            g_RenderManager.SetFontStyle(PSP_MENU_SIZE, 0xFFFFFFFF, 0, INTRAFONT_ALIGN_CENTER, 0.0f);
+        }
+        g_RenderManager.DebugPrint(240, 256, Common::g_TranslationOBJ.getText("gui.done").c_str());
 
     }
 
 	void MenuState::controlUpdate(){
+
+        if(Input::KeyPressed(PSP_CTRL_DOWN)){
+            g_AudioManager.PlaySound(button, AUDIO_CHANNEL_GUI);
+            selectPosY++;
+            if(selectPosY > 16){
+                selectPosY = 16;
+            }
+        }
+        
+        if(Input::KeyPressed(PSP_CTRL_UP)){
+            g_AudioManager.PlaySound(button, AUDIO_CHANNEL_GUI);
+            selectPosY--;
+            if(selectPosY < 0){
+                selectPosY = 0;
+            }
+        }
+
+        if(Input::KeyPressed(PSP_CTRL_CROSS) && selectPosY == 16){
+            Input::writeConfiguration("config.json");
+
+            selectPosX = 0;
+            selectPosY = 0;
+            selectRegion = 0;
+            previous_states = menu_states;
+            menu_states = MENU_STATE_OPTIONS_MAIN;
+        }
 
     }
 }
