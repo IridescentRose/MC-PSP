c4c65f5fd98014f9bb18a54ff0b80be097c85149
Nathan Bourgeois <iridescentrosesfall@gmail.com>
2020-03-23 19:32:05 -0400

    FULL STARDUST!

--- Minecraft/Minecraft.cpp
+++ Minecraft/Minecraft.cpp
@@ -1,87 +1,68 @@
-#include <math.h>
-#include <psptypes.h>
-
-#include <Shadow/Graphics/RenderManager.h>
-#include <Shadow/Utils/StateManager.h>
-#include <Shadow/Graphics/TextureUtil.h>
 
 #include <Platform/Platform.h>
-
-#include "Minecraft/Menu/Logo.hpp"
+#include <Graphics/TextureUtil.h>
+#include <Graphics/2D/SpriteBase.h>
+#include <Utilities/Logger.h>
+#include <Utilities/Timer.h>
+#include <Graphics/RendererCore.h>
 #include "Minecraft/Version.hpp"
-#include "Minecraft/Menu/MenuState.hpp"
-using namespace Minecraft::Menus;
-
-using namespace Shadow::Graphics;
-using namespace Shadow::Utils;
-using namespace Shadow;
 
 PSP_MODULE_INFO("Minecraft", 0, VERSION_MAJOR, VERSION_MINOR);
 PSP_MAIN_THREAD_ATTR(THREAD_ATTR_VFPU|THREAD_ATTR_USER);
 PSP_HEAP_SIZE_KB(-256);
 
-void initGame(){
-	//initialize render manager
-	{
-		g_RenderManager.Init();
-		g_RenderManager.CollectPerformance(true);
-		g_RenderManager.InitDebugFont();
-		g_RenderManager.SetClearColour(0xFF000000);
-	}
-	//set perspectives
-	{
-		sceGumMatrixMode(GU_PROJECTION);
-		sceGumLoadIdentity();
-		sceGumOrtho(0, 480, 272, 0, -30, 30);
-
-		sceGumMatrixMode(GU_VIEW);
-		sceGumLoadIdentity();
-
-		sceGumMatrixMode(GU_MODEL);
-		sceGumLoadIdentity();
-	}
-
-	VirtualFileInit();
-	oslInitAudio();
-}
 
 void doSplash(){
+	//2D Mode initialization
+	sceGumMatrixMode(GU_PROJECTION);
+	sceGumLoadIdentity();
+	sceGumOrtho(0, 480, 272, 0, -30, 30);
+	sceGumMatrixMode(GU_VIEW);
+	sceGumLoadIdentity();
+	sceGumMatrixMode(GU_MODEL);
+	sceGumLoadIdentity();
+
+	Stardust::Graphics::g_RenderCore.SetClearColor(1.0f, 1.0f, 1.0f, 1.0f);
+
 	//splash screen!
-	{
-		Texture* tex = TextureUtil::LoadPng("./assets/minecraft/textures/gui/title/mojang.png");
-		Logo* logo = new Logo();
-		logo->render(tex, GU_COLOR(1.0f, 1.0f, 1.0f, 1.0f), 4.0f);
-		delete logo;
-		delete tex;
+	Stardust::Graphics::Texture* tex = Stardust::Graphics::TextureUtil::LoadPng("./assets/minecraft/textures/gui/title/mojang.png");
+	
+	Stardust::Graphics::Render2D::Sprite* sprite = new Stardust::Graphics::Render2D::Sprite(tex);
+	sprite->SetPosition(240, 136);
+	sprite->Scale(1.0f, 1.0f);
+
+	float time = 3.0f;
+	Stardust::Utilities::Timer* timer = new Stardust::Utilities::Timer();
+	timer->reset();
+
+	while(time > 0.0f){
+		Stardust::Graphics::g_RenderCore.BeginCommands();
+		Stardust::Graphics::g_RenderCore.Clear();
+
+		time -= timer->deltaTime();
+		sprite->Draw();
+
+		Stardust::Graphics::g_RenderCore.EndCommands();
 	}
+	
+	delete sprite;
+	delete timer;
+	delete tex;
 }
 
-void exitGame(){
-	sceGuTerm();
-	sceKernelExitGame();
-}
 
 void mainLoop(){
-	StateManager stateManager;
 	
-	MenuState *state = new MenuState();
-	state->Init();
-	stateManager.ChangeState(state);
-
-	while ( true )
-	{
-		stateManager.Update();
-		stateManager.Draw();
-	}
 }
+
 
 int main()
 {
-	initGame();
+	Stardust::Platform::initPlatform();
 	doSplash();
 
 	mainLoop();
 
-	exitGame();
+	Stardust::Platform::exitPlatform();
 	return 0;
 }
